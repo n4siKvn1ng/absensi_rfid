31c66dbeca333da3663c887ce9a4b2432a7342e8
FaizalTrianto03 <128003049+FaizalTrianto03@users.noreply.github.com>
2023-03-23 11:03:56 +0700

    memberikan highlight pada tab yang sedang dibuka

--- menu.php
+++ menu.php
@@ -1,13 +1,13 @@
 <nav class="navbar navbar-inverse">
-    <div class="container-fluid">
+    <div class="container-fluid" style="color: white">
         <div class="navbar-header">
             <a href="#" class="navbar-brand">ABSENSI</a>
         </div>
         <ul class="nav navbar-nav">
-            <li> <a href="index.php">HOME </a> </li>
-            <li> <a href="datamahasiswa.php"> Data Mahasiswa </a> </li>
-            <li> <a href="absensi.php"> Rekapitulasi Absensi </a> </li>
-            <li> <a href="scan.php"> Scan Kartu </a> </li>
+            <li <?php if(basename($_SERVER['PHP_SELF'])=='index.php'){echo 'class="active"';} ?>> <a href="index.php">HOME </a> </li>
+            <li <?php if(in_array(basename($_SERVER['PHP_SELF']), ['datamahasiswa.php', 'edit.php', 'tambah.php'])){echo 'class="active"';} ?>> <a href="datamahasiswa.php"> Data Mahasiswa </a> </li>
+            <li <?php if(basename($_SERVER['PHP_SELF'])=='absensi.php'){echo 'class="active"';} ?>> <a href="absensi.php"> Rekapitulasi Absensi </a> </li>
+            <li <?php if(basename($_SERVER['PHP_SELF'])=='scan.php'){echo 'class="active"';} ?>> <a href="scan.php"> Scan Kartu </a> </li>
         </ul>
     </div>
 </nav>
