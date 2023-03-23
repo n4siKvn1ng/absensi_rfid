da2561f3d9d40f58a55f01021a8ecdd4e0d88262
FaizalTrianto03 <128003049+FaizalTrianto03@users.noreply.github.com>
2023-03-23 10:58:16 +0700

    menambahkan kondisi
    menambahkan kondisi untuk mengecek data, dan mengatur kolom kartu rfid hanya untuk dibaca saja.

--- edit.php
+++ edit.php
@@ -2,12 +2,13 @@
 
 <?php include "koneksi.php";
 
-    // baca ID data yang akan diedit
+    //baca ID Data yang akan di edit
     $id = $_GET['id'];
 
-    // baca data mahasiswa berdasarkan id
-    $cari = mysqli_query($konek, "select * from mahasiswa where id='$id'");
-    $hasil = mysqli_fetch_array($cari);
+    //baca Data Mahasiswa berdasarkan id
+     $cari = mysqli_query($konek, "SELECT * FROM mahasiswa WHERE id='$id'");
+     $hasil = mysqli_fetch_array($cari);
+
 
     //kondisi jika tombol simpan diklik
     if(isset($_POST['btnSimpan']))
@@ -17,32 +18,56 @@
         $nim        = $_POST['nim'];
         $nama       = $_POST['nama'];
 
-        //simpan ke tabel mahasiswa
-        $simpan = mysqli_query($konek, "update mahasiswa set nokartu='$nokartu', nim='$nim', nama='$nama' where id='$id'");
-
-        //jika berhasil tersimpan, tampilkan pesan Tersimpan
-        //kembali ke data mahasiswa
-        if($simpan)
-        {
+        //mengecek apakah ada data yang sama
+        $cek_nokartu = mysqli_query($konek, "SELECT * FROM mahasiswa WHERE nokartu='$nokartu'");
+        $cek_nim = mysqli_query($konek, "SELECT * FROM mahasiswa WHERE nim='$nim'");
+        $cek_nama = mysqli_query($konek, "SELECT * FROM mahasiswa WHERE nama='$nama'");
+        if(mysqli_num_rows($cek_nim) > 0) {
             echo "
                 <script>
-                    alert('Data Berhasil Tersimpan');
-                    location.replace('datamahasiswa.php');
+                    alert('Data Mahasiswa dengan NIM yang sama sudah tersimpan');
                 </script>
             ";
-        }
-        else 
-        {
+        }else if (mysqli_num_rows($cek_nama) > 0) {
             echo "
                 <script>
-                    alert('Data Gagal Tersimpan');
-                    location.replace('datamahasiswa.php');
+                    alert('Data Mahasiswa dengan Nama yang sama sudah tersimpan');
                 </script>
             ";
+        }else {
+            //simpan ke tabel mahasiswa
+            $simpan = mysqli_query($konek, "update mahasiswa set nokartu='$nokartu', nim='$nim', nama='$nama' where id='$id'");
+            
+            if($simpan)
+            {
+                echo "
+                    <script>
+                        alert('Data Berhasil Tersimpan');
+                        location.replace('datamahasiswa.php');
+                    </script>
+                ";
+            }
+            else 
+            {
+                echo "
+                    <script>
+                        alert('Data Gagal Tersimpan');
+                        location.replace('datamahasiswa.php');
+                    </script>
+                ";
+            }   
         }
-    }
-
-    ?>
+        
+     } 
+     if(isset($_POST['batal'])){
+       
+        echo "
+            <script>
+                location.replace('datamahasiswa.php');
+            </script>
+            ";
+        }
+?>
 
 
 <!DOCTYPE html>
@@ -50,6 +75,8 @@
     <head>
         <?php include "header.php"; ?>
         <title>Tambah Data Mahasiswa</title>
+        <link rel="stylesheet" type="text/css" href="style.css">
+
     </head>
     <body>
 
@@ -57,7 +84,7 @@
 
     <!-- isi-->
     <div class="container-fluid">
-        <h3>Tambah Data Mahasiswa</h3>
+        <h3>Edit Data Mahasiswa</h3>
 
         <!-- form input -->
         <form method="POST">
@@ -65,26 +92,31 @@
             <!--INPUTAN UNTUK NOMOR KARTU-->
             <div class="form-group">
                 <label>No. Kartu</label>
-                <input type="text" name="nokartu" id="nokartu" placeholder="Nomor kartu RFID" class="form-control" style="width: 200px"
-                value="<?php echo $hasil['nokartu']; ?>">
+                <input type="text" name="nokartu" id="nokartu" placeholder="Nomor kartu RFID" class="form-control" style="width: 200px" required
+                value="<?php echo htmlspecialchars($hasil['nokartu']); ?>" readonly>
             </div>
 
             <!--INPUTAN UNTUK NOMOR INDUK MAHASISWA-->
             <div class="form-group">
                 <label>Nomor Induk Mahasiswa (NIM)</label>
-                <input type="text" name="nim" id="nim" placeholder="Nomor Induk Mahasiswa" class="form-control" style="width: 200px" value="<?php echo $hasil['nim']; ?>">
+                <input type="text" name="nim" id="nim" placeholder="Nomor Induk Mahasiswa" class="form-control" style="width: 200px" required
+                value="<?php echo $hasil['nim']; ?>">
             </div>
 
             <!--INPUTAN UNTUK NAMA MAHASISWA-->
             <div class="form-group">
                 <label>Nama Mahasiswa</label>
-                <input type="text" name="nama" id="nama" placeholder="Nama Mahasiswa" class="form-control" style="width: 400px" value="<?php echo $hasil['nama']; ?>">
+                <input type="text" name="nama" id="nama" placeholder="Nama Mahasiswa" class="form-control" style="width: 400px"required
+                value="<?php echo $hasil['nama']; ?>">
             </div>
 
-            <button class="btn btn-primary" name="btnSimpan" >Simpan</button>
+            <button class="btn btn-primary" name="btnSimpan" >Update</button>
+            <button class="btn btn-warning" name="batal" onclick="location.href='datamahasiswa.php'">Batal</button>
+            
         </form>
     </div>
 
     <?php include "footer.php"; ?>
     </body>
 </html>
+
