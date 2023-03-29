c1110aeaac4e5f11ffa0e1e5d13d87729ddf5263
FaizalTrianto03 <128003049+FaizalTrianto03@users.noreply.github.com>
2023-03-29 10:22:43 +0700

    Menambahkan file baru bernama no kartu

---
+++ nokartu.php
@@ -0,0 +1,24 @@
+<?php
+    include "koneksi.php";
+
+    //saya menggunakan pengkodisian dikarenakan mencegah tampilan error pada website dikarenakan data dianggap null
+
+    //Baca isi tabel tmprfid
+$sql = mysqli_query($konek, "SELECT * from `tmprfid`  ");
+if (mysqli_num_rows($sql) > 0) {
+    $data = mysqli_fetch_array($sql);
+    //Membaca No Kartu
+    $nokartu = $data['nokartu'];
+} else {
+    $nokartu = ""; // Atau bisa juga menggunakan nilai default lainnya
+    echo "<strong><font style=color: red>Kolom No. Kartu tidak boleh kosong.</font></strong>"; // Tampilkan pesan jika data tidak ditemukan
+}
+?>
+ 
+ <!--INPUTAN UNTUK NOMOR KARTU-->
+ <div class="form-group">
+        <label>No. Kartu</label>
+        <input type="text" name="nokartu" id="nokartu" placeholder="Tempelkan kartu RFID" class="form-control" style="width: 200px"required
+        value="<?php echo $nokartu;?>" >
+</div>
+
