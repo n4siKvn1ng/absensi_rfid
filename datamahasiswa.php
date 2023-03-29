0007a0c1eb2aa7f3678074758004e612645aba47
FaizalTrianto03 <128003049+FaizalTrianto03@users.noreply.github.com>
2023-03-29 10:18:35 +0700

    Menambahkan fitur pencarian data

--- datamahasiswa.php
+++ datamahasiswa.php
@@ -4,10 +4,24 @@
         <?php include "header.php"; ?>
         <title>Data Mahasiswa</title>
        <link rel="stylesheet" type="text/css" href="style.css">
+       <style>
+        #hapusTeks {
+            display: none;
+        }
+        <?php
+        if(isset($_GET['q']) && $_GET['q'] != '') {
+            echo '#hapusTeks { display: block; }';
+        } else {
+            echo '#hapusTeks { display: none; }';
+        }
+        ?>
+    </style>
     </head>
     <body>
 
         <?php include "menu.php"; ?>
+        
+        
 
         <!--ISI-->
         
@@ -15,15 +29,50 @@
         <div class="container-fluid" style="width:100%;">
             <h3>Data Mahasiswa</h3>
 
+            
+            <!-- form pencarian -->
+            <div class="table-header" style="float: left; margin-right: 0;">
+                <form method="GET" action="" class="form-inline">
+                    <div class="input-group">
+                        <label for="inputSearch" class="sr-only">Cari:</label>
+                            <input type="text" class="form-control" id="inputSearch" name="q" placeholder="Masukkan kata kunci" style="width: 400px"
+                            value="<?php echo isset($_GET['q']) ? $_GET['q'] : '' ?>">
+                                <div class="input-group-addon">
+                                <span id="hapusTeks" style="cursor: pointer;">X</span>
+                                </div>
+                        </div>
+                    <button type="submit" class="btn btn-primary">Cari</button>
+                </form>
+            </div>
+
+                <script>
+                    
+                document.getElementById("inputSearch").addEventListener("input", function() {
+                    if (this.value !== "") {
+                    document.getElementById("hapusTeks").style.display = "block";
+                    } else {
+                    document.getElementById("hapusTeks").style.display = "none";
+                    }
+                });
+                
+                document.getElementById("hapusTeks").addEventListener("click", function() {
+                    document.getElementById("inputSearch").value = "";
+                    this.style.display = "none";
+                    document.querySelector('form').submit();
+                });
+                </script>
+
+
                         
                         <!-- tombol tambah data mahasiswa -->
                         <div class="table-header" style="float: right;">
-                        <a href="tambah.php"><button class="btn btn-primary"> Tambah Data Mahasiswa</button></a>
+                            <a href="tambah.php"><button class="btn btn-primary"> Tambah Data Mahasiswa</button></a>
                         </div>
 
                         <div style="clear: both;"></div>
 
-            <table class="table table-bordered" style="margin-top: 10px">
+                        <table class="table table-bordered" style="margin-top: 10px; ">
+
             <thead>
                 <tr style="background-color: grey; color: white; height:100%;">
                     <th style="width: 10px; text-align: center" >No.</th>
@@ -35,33 +84,39 @@
             </thead>
             <tbody>
 
-                <?php
-                    // koneksi ke database
-                    include "koneksi.php";
+            <?php
+        // koneksi ke database
+        include "koneksi.php";
 
-                    // baca data mahasiswa
-                    $sql = mysqli_query($konek, "select * from mahasiswa");
-                    $no = 0;
-                    while($data = mysqli_fetch_array($sql))
-                    {
-                        $no++;
-                ?>
+        // cek apakah ada data yang dicari
+        if(isset($_GET['q']) && $_GET['q'] != '') {
+            $keyword = $_GET['q'];
+            // query pencarian data mahasiswa
+            $sql = mysqli_query($konek, "SELECT * FROM mahasiswa WHERE nim LIKE '%$keyword%' OR nama LIKE '%$keyword%'");
+        } else {
+            // query menampilkan seluruh data mahasiswa
+            $sql = mysqli_query($konek, "SELECT * FROM mahasiswa");
+        }
 
-                <tr>
-                    <td style="text-align: center"> <?php echo $no; ?> </td>
-                    <td> <?php echo $data['nokartu']; ?> </td>
-                    <td> <?php echo $data['nim']; ?> </td>
-                    <td> <?php echo $data['nama']; ?> </td>
-                    
-                    
-                    <td>
-                        <a href="edit.php?id=<?php echo $data['id']; ?>" ><button class="btn btn-success" style="width:70x;">Edit</button>
-                        </a> <a href="hapus.php?id=<?php echo $data['id']; ?>" ><button class="btn btn-danger" style="width:65px;">Hapus</button></a>
-                       
-                    </td>
-                </tr>
+        $no = 0;
+        while($data = mysqli_fetch_array($sql))
+        {
+            $no++;
+        ?>
 
-                <?php } ?>
+        <tr>
+            <td style="text-align: center"> <?php echo $no; ?> </td>
+            <td> <?php echo $data['nokartu']; ?> </td>
+            <td> <?php echo $data['nim']; ?> </td>
+            <td> <?php echo $data['nama']; ?> </td>
+            <td>
+                <a href="edit.php?id=<?php echo $data['id']; ?>" ><button class="btn btn-success" style="width:70x;">Edit</button></a>
+                <a href="hapus.php?id=<?php echo $data['id']; ?>" ><button class="btn btn-danger" style="width:65px;">Hapus</button></a>
+            </td>
+        </tr>
+
+        <?php } ?>
+
             </tbody>
             </table>
 
@@ -71,7 +126,9 @@
         
     </body>
 
-    <?php include "footer.php";  ?>
+    <?php include "footer.php"?>
    
 </html> 
+
+
 
