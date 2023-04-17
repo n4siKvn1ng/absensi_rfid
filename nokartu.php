193d850a86b476e58427a729cc6f097e0ae635fb
radan775 <90452459+radan775@users.noreply.github.com>
2023-04-17 14:53:43 +0700

    penambahan kolom dan kolom jadi readonly

--- nokartu.php
+++ nokartu.php
@@ -1,36 +1,27 @@
 <?php
     include "koneksi.php";
-
-    //saya menggunakan pengkodisian dikarenakan mencegah tampilan error pada website dikarenakan data dianggap null
-
     //Baca isi tabel tmprfid
-$sql = mysqli_query($konek, "SELECT * from tmprfiddaftar ");
-if (mysqli_num_rows($sql) > 0) {
-    $data = mysqli_fetch_array($sql);
-    //Membaca No Kartu
-    $nokartu = $data['nokartu'];
-} 
+    $sql = mysqli_query($konek, "SELECT * from tmprfiddaftar ");
+    if (mysqli_num_rows($sql) > 0) {
+        $data = mysqli_fetch_array($sql);
+        //Membaca No Kartu
+        $nokartu = $data['nokartu'];
+    } 
 ?>
 <!-- INPUTAN UNTUK NOMOR KARTU -->
 <div class="form-group">
-    <label>Nomor Induk Mahasiswa (NIM)</label>
+    <label>Nomor Kartu Sekarang</label>
     <?php
-    $sql = mysqli_query($konek, "SELECT * from `tmprfiddaftar` ");
-    if (mysqli_num_rows($sql) >= 0) {
-        $data = mysqli_fetch_array($sql);
-        if (empty($data['nokartu'])) {
-            ?>
-            <div class="input-group">
-                <input type="text" name="nokartu" id="nokartu" placeholder="Harap Menempelkan Kartu" class="form-control" style="width: 250px" required >
-            </div>
-            <?php
+    $cari = mysqli_query($konek, "SELECT * FROM `tmprfiddaftar` ");
+    $hasil = mysqli_fetch_array($cari);
+        if (empty($hasil['nokartu'])) {
+            echo '<div class="input-group">
+                    <input type="text" name="tampilnomor" id="nokartu" placeholder="Harap Menempelkan Kartu" class="form-control" style="width: 250px" readonly>
+                </div>';
         } else {
-            ?>
-            <div class="input-group">
-                <input type="text" name="nokartu" id="nokartu" placeholder="Ubah No. Kartu sesuai NIM" class="form-control" style="width: 250px" required value="<?php echo $data['nokartu']; ?>">
-            </div>
-            <?php
+            echo '<div class="input-group">
+                    <input type="text" name="tampilnomor" id="nokartu" placeholder="Ubah No. Kartu sesuai NIM" class="form-control" style="width: 250px" readonly value="' . $hasil['nokartu'] . '">
+                </div>';
         }
-    }
     ?>
 </div>
