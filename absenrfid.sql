ff3038691585911d32512d736b9f5a0e1a25ebf2
DarmaPutra0 <110342910+DarmaPutra0@users.noreply.github.com>
2023-05-11 15:56:00 +0700

    Add files via upload

--- absenrfid.sql
+++ absenrfid.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: May 11, 2023 at 09:02 AM
+-- Generation Time: May 11, 2023 at 10:53 AM
 -- Server version: 10.4.27-MariaDB
 -- PHP Version: 8.1.12
 
@@ -41,8 +41,8 @@ CREATE TABLE `absensi` (
 --
 
 INSERT INTO `absensi` (`id`, `nokartu`, `id_kelas`, `tanggal`, `jam_absensi`, `keterangan`) VALUES
-(62, '202210370311408', 1, '2023-05-11', '12:28:01', 'Terlambat - -13 menit- -'),
-(63, '202210370311406', 3, '2023-05-11', '12:30:25', 'Terlambat - -15 menit- -');
+(73, '202210370311375', 1, '2023-05-11', '15:33:18', 'Terlambat - -18 menit- -'),
+(74, '202210370311408', 1, '2023-05-11', '15:35:34', 'Terlambat - -21 menit- -');
 
 -- --------------------------------------------------------
 
@@ -54,22 +54,23 @@ CREATE TABLE `kelas` (
   `id_kelas` int(2) NOT NULL,
   `kelas_praktikum` varchar(30) NOT NULL,
   `hari` varchar(20) NOT NULL,
-  `jam` time NOT NULL
+  `jam` time NOT NULL,
+  `id_lab` int(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `kelas`
 --
 
-INSERT INTO `kelas` (`id_kelas`, `kelas_praktikum`, `hari`, `jam`) VALUES
-(1, 'PBO A', 'Thursday', '12:00:00'),
-(2, 'PBO B', 'Tuesday', '09:00:00'),
-(3, 'PROGDAS A', 'Thursday', '12:00:23'),
-(4, 'PROGDAS B', 'Wednesday', '00:20:23'),
-(5, 'PROGDAS C', 'Thursday', '13:00:00'),
-(6, 'PROGDAS G', 'Friday', '00:00:00'),
-(7, 'STRUKTUR DATA E', 'Thursday', '15:00:00'),
-(8, 'PBO C', 'Saturday', '20:24:52');
+INSERT INTO `kelas` (`id_kelas`, `kelas_praktikum`, `hari`, `jam`, `id_lab`) VALUES
+(1, 'PBO A', 'Thursday', '15:00:00', 1),
+(2, 'PBO B', 'Tuesday', '09:00:00', 0),
+(3, 'PROGDAS A', 'Thursday', '12:00:23', 0),
+(4, 'PROGDAS B', 'Wednesday', '00:20:23', 0),
+(5, 'PROGDAS C', 'Thursday', '13:00:00', 0),
+(6, 'PROGDAS G', 'Friday', '00:00:00', 0),
+(7, 'STRUKTUR DATA E', 'Thursday', '15:00:00', 0),
+(8, 'PBO C', 'Saturday', '20:24:52', 0);
 
 -- --------------------------------------------------------
 
@@ -80,6 +81,26 @@ INSERT INTO `kelas` (`id_kelas`, `kelas_praktikum`, `hari`, `jam`) VALUES
 CREATE TABLE `kirimdata` (
   `nokartu` varchar(15) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `lab`
+--
+
+CREATE TABLE `lab` (
+  `id_lab` int(1) NOT NULL,
+  `nama_lab` varchar(6) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `lab`
+--
+
+INSERT INTO `lab` (`id_lab`, `nama_lab`) VALUES
+(1, 'Lab AB'),
+(2, 'Lab CD'),
+(3, 'Lab EF');
 
 -- --------------------------------------------------------
 
@@ -99,25 +120,7 @@ CREATE TABLE `mahasiswa` (
 --
 
 INSERT INTO `mahasiswa` (`id`, `nokartu`, `nama`, `id_kelas`) VALUES
-(57, '202210370311408', 'Zul Razak', '1'),
-(58, '202210370311406', 'Abdul Salam', '3');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `nolab`
---
-
-CREATE TABLE `nolab` (
-  `lab` varchar(2) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Dumping data for table `nolab`
---
-
-INSERT INTO `nolab` (`lab`) VALUES
-('');
+(69, '202210370311408', 'Maulana Ikhsan', '1');
 
 -- --------------------------------------------------------
 
@@ -151,7 +154,7 @@ CREATE TABLE `tmprfiddaftar` (
 --
 
 INSERT INTO `tmprfiddaftar` (`nokartu`) VALUES
-('219942953');
+('171523153');
 
 -- --------------------------------------------------------
 
@@ -160,15 +163,16 @@ INSERT INTO `tmprfiddaftar` (`nokartu`) VALUES
 --
 
 CREATE TABLE `tmprfidscan` (
-  `nokartu` varchar(15) NOT NULL
+  `nokartu` varchar(15) NOT NULL,
+  `no_lab` varchar(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `tmprfidscan`
 --
 
-INSERT INTO `tmprfidscan` (`nokartu`) VALUES
-('202210370311406');
+INSERT INTO `tmprfidscan` (`nokartu`, `no_lab`) VALUES
+('202210370311408', '1');
 
 --
 -- Indexes for dumped tables
@@ -193,16 +197,16 @@ ALTER TABLE `kirimdata`
   ADD PRIMARY KEY (`nokartu`);
 
 --
+-- Indexes for table `lab`
+--
+ALTER TABLE `lab`
+  ADD PRIMARY KEY (`id_lab`);
+
+--
 -- Indexes for table `mahasiswa`
 --
 ALTER TABLE `mahasiswa`
   ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `nolab`
---
-ALTER TABLE `nolab`
-  ADD PRIMARY KEY (`lab`);
 
 --
 -- Indexes for table `status`
@@ -230,13 +234,13 @@ ALTER TABLE `tmprfidscan`
 -- AUTO_INCREMENT for table `absensi`
 --
 ALTER TABLE `absensi`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=64;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=75;
 
 --
 -- AUTO_INCREMENT for table `mahasiswa`
 --
 ALTER TABLE `mahasiswa`
-  MODIFY `id` int(15) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=59;
+  MODIFY `id` int(15) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=70;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
